227933c2612ea053d456ce6b27a4f34c42a33327
Surafeljava <surafelk27@gmail.com>
2021-04-15 23:12:56 +0300

    Creating caesar encryption logic

--- lib/caesar_enc.dart
+++ lib/caesar_enc.dart
@@ -1,15 +1,28 @@
 class CaesarEnc{
 
-  static void getEncryptedString(String value){
+  static String getEncryptedString(String value, int shift){
 
-    String s = "Hello World";
+    String encrypted = "";
 
-    for (int i = 0; i < s.length; i++) {
-      print("Code unit for ${s[i]} is ${s.codeUnitAt(i)}");
+    for (int i = 0; i < value.length; i++) {
+
+      if(value[i].toUpperCase()==value[i]){
+        int charCode = value.codeUnitAt(i);
+
+        int newCharCode = (charCode + shift - 65) % 26 + 65;
+        String val = String.fromCharCode(newCharCode);
+        encrypted += val;
+      }else{
+        int charCode = value.codeUnitAt(i);
+
+        int newCharCode = (charCode + shift - 97) % 26 + 97;
+        String val = String.fromCharCode(newCharCode);
+        encrypted += val;
+      }
+
     }
 
-    print("Code unit list ${s.codeUnits}");
-
+    return encrypted;
 
   }
 
--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:caesarencryption/caesar_enc.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,27 +10,140 @@ class _HomeState extends State<Home> {
 
   TextEditingController textEditingController = new TextEditingController();
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool validate = false;
+
+  int shift = 3;
+
+  String encrypted = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text('Caesar Encryption', style: TextStyle(color: Colors.grey[800], fontSize: 25.0, fontWeight: FontWeight.w600, letterSpacing: 1.0),),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-              child: TextFormField(
-                controller: textEditingController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: 'Type here'
+      key: _scaffoldKey,
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Form(
+            key: _formKey,
+            autovalidate: validate,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Spacer(),
+                Text('Caesar Encryption', style: TextStyle(color: Colors.grey[800], fontSize: 28.0, fontWeight: FontWeight.w600, letterSpacing: 1.0),),
+                SizedBox(height: 20.0,),
+                Text('$encrypted', style: TextStyle(color: Colors.grey[700], fontSize: 20.0, fontWeight: FontWeight.w400, letterSpacing: 1.0),),
+                Spacer(),
+                Text('Shift By:', style: TextStyle(color: Colors.grey[700], fontSize: 18.0, fontWeight: FontWeight.w400, letterSpacing: 1.0),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove, color: Colors.redAccent, size: 30.0,),
+                      onPressed: (){
+                        if(shift>2){
+                          setState(() {
+                            shift -= 1;
+                          });
+                        }else{
+                          setState(() {
+                            shift = shift;
+                          });
+                        }
+                      },
+                    ),
+                    SizedBox(width: 10.0,),
+                    Text('$shift', style: TextStyle(color: Colors.grey[800], fontSize: 30.0, fontWeight: FontWeight.w500, letterSpacing: 1.0),),
+                    SizedBox(width: 10.0,),
+                    IconButton(
+                      icon: Icon(Icons.add, color: Colors.blue, size: 30.0,),
+                      onPressed: (){
+                        if(shift<25){
+                          setState(() {
+                            shift += 1;
+                          });
+                        }else{
+                          setState(() {
+                            shift = shift;
+                          });
+                        }
+                      },
+                    ),
+                  ],
                 ),
-              ),
+
+                SizedBox(height: 20.0,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  child: TextFormField(
+                    controller: textEditingController,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      hintText: 'Type here'
+                    ),
+                    validator: (val){
+                      if(val.isEmpty){
+                        return "Please enter your text first";
+                      }{
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ElevatedButton(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 12.0),
+                      child: Text('Encrypt'.toUpperCase(), style: TextStyle(fontWeight: FontWeight.w400, letterSpacing: 1.0),),
+                    ),
+                    onPressed: (){
+
+                      setState(() {
+                        validate = true;
+                      });
+
+                      if(_formKey.currentState.validate()){
+
+                        String enc = CaesarEnc.getEncryptedString(textEditingController.text, shift);
+
+                        setState(() {
+                          encrypted = enc;
+                        });
+
+                      }else{
+                        print("Please check ur input");
+                      }
+                    },
+                  ),
+                ),
+
+                Spacer(),
+
+
+                ElevatedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12.0),
+                    child: Text("Read about 'Caesar Encryption'", style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400, letterSpacing: 1.0),),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey[800], // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                  onPressed: (){
+
+                  },
+                ),
+
+                SizedBox(height: 10.0,),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
